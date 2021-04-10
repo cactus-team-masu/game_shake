d5bf76bab1900760d86aa4a6f5bb82a4ad0cb7ee
ZhPolina <polina.zheleznicova@yandex.ru>
2021-04-10 19:07:21 +0300

    Скрипты для змейки.

---
+++ Shake_2/Assets/Script/Bonus.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bonus : MonoBehaviour
+{
+
+	public float timeout = 10;
+
+	void Start()
+	{
+		Destroy(gameObject, timeout);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Snake.tailCount++;
+		Destroy(gameObject);
+	}
+}
